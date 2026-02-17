f451aedd4ebd4afcf3daa3ea667adb3f79d0d21d
prohd05 <merques.enoe@gmail.com>
2026-02-17 13:28:23 +0000

    Fixed Logo in Navbar

--- app/comp/navbar.tsx
+++ app/comp/navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function navbar(){
     return(
         <div className="navbar">
-            <Link href='/'> <Picture alt="logo" src="/logo.png" width={95} height={95}/> </Link>
+            <Link href='/'> <Picture alt="logo" src="/Logo.png" width={95} height={95}/> </Link>
             <ul className="links"> 
                 <Link href='/projects' className="heading4"> Work </Link>
                 <Link href='/contact' className="heading4"> About </Link>
